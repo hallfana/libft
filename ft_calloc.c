898d02aabee6d84472a2fee602b897b77e4dbe60
hallfana <hallfana@proton.me>
2024-10-23 18:09:31 +0200

    auto update

--- ft_calloc.c
+++ ft_calloc.c
@@ -7,7 +7,10 @@ void *ft_calloc(size_t nmemb, size_t size)
   if (nmemb * size >= 2147483647 || ((int)nmemb < 0 && (int)size < 0))
     return (NULL);
   if (nmemb == 0 || size == 0)
-    return (malloc(1));
+  {
+    ptr = malloc(1);
+    return (ptr);
+  }
   ptr = malloc(nmemb * size);
   return (ptr);
 }
