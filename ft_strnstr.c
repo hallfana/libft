84afc55fb080c710b32b21eef950917e91cb3e8b
hallfana <hallfana@proton.me>
2024-10-23 17:00:30 +0200

    auto update

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -7,8 +7,6 @@ char  *ft_strnstr(const char *big, const char *little, size_t len)
 
   i = 0;
   l = 0;
-  if (ft_strlen(big) == 0)
-    return (0);
   if (ft_strlen(little) == 0 || (int)len < 0)
     return ((char *)big);
   while (big[i] && i < len)
