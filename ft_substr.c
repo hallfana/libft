96bba82dcfc14cd2be2c08af7fe36481cf14c9f8
hallfana <hallfana@proton.me>
2024-10-23 18:57:40 +0200

    auto update

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,10 @@ char  *ft_substr(char const *s, unsigned int start, size_t len)
   size_t  j;
   char    *str;
 
-  str = (char*)malloc(sizeof(*s) * (len + 1));
+  if (ft_strlen(s) > len)
+    str = (char*)malloc(sizeof(*s) * (len + 1));
+  else
+    str - (char*)malloc(sizeof(*s) * (ft_strlen(s) + 1));  
   if (!str)
     return (NULL);
   i = 0;
