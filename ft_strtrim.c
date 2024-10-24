b33032be14bb93d15d723a04965d5a03db0b5f6f
hallfana <hallfana@proton.me>
2024-10-24 16:33:15 +0200

    auto update

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -1,5 +1,4 @@
 #include "./libft.h"
-//#include <stdio.h>
 
 int ft_incharset(const int c, const char *charset)
 {
@@ -23,14 +22,5 @@ char  *ft_strtrim(char const *s, char const *charset)
   j = (int)ft_strlen(s);
   while (s[++i] && ft_incharset(s[i], charset) == 1);
   while (s[--j] && ft_incharset(s[j], charset) == 1);
-  //printf("\nfound char from charset at pos i:%d j:%d len:%d in %s\n", i, j, j-i,s);
-  //printf("got result: %s\n", ft_substr(s, i + 1, j - i));
   return (ft_substr(s, i, (j+1) - i));
 }
-
-/*int main(int argc, char **argv)
-{
-  (void)argc;
-  ft_strtrim(argv[1], argv[2]);
-  return (0);
-}*/
