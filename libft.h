bcfc9ac90123f6c5ca7c868026a2fe808b87c134
hallfana <hallfana@proton.me>
2024-10-24 16:25:46 +0200

    auto update

--- libft.h
+++ libft.h
@@ -32,5 +32,6 @@ void *ft_calloc(size_t nmemb, size_t size);
 char  *ft_strdup(const char *s);
 char  *ft_substr(char const *s, unsigned int start, size_t len);
 char  *ft_strjoin(char const *s1, char const *s2);
+char  *ft_strtrim(char const *s, char const *charset);
 
 #endif
