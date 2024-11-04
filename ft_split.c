20e3e7e49d2f872f150d5e180e894c572b8b0890
Samuel Bernard <samberna@c2r7p3.42perpignan.fr>
2024-11-04 14:07:05 +0100

    auto update

--- ft_split.c
+++ ft_split.c
@@ -26,14 +26,30 @@ int ft_countwords(char const *s, int c)
 
 char  **ft_split(char const *s, char c)
 {
-  (void)s;
-  (void)c;
-  printf("%d", ft_countwords("asdasda*sd*", (int)'*'));
-  return NULL;
+  int     i;
+  int     s;
+  int     e;
+  char**  dst;
+
+  dst = (char**)malloc(sizeof(char*) * (ft_countwords(s, (int)c) + 1));
+  if (!dst)
+    return (NULL);
+  i = 0;
+  s = 0;
+  e = 0;
+  while (i < ft_countwords(s, (int)c))
+  {
+    while (s[s] == c)
+      s++;
+    e = s;
+    while (s[e] != c)
+      e++;
+    i++;
+  }
 }
 
 int main(void)
 {
-  ft_split("asd", 'a');
+  ft_split("aaaaa6g4dab56g4aaa1b65tya14rty56bar14sd", 'a');
   return (0);
 }
--- ft_strdup.c
+++ ft_strdup.c
@@ -17,3 +17,21 @@ char  *ft_strdup(const char *s)
   c[i] = '\0';
   return c;
 }
+
+char  *ft_strndup(const char *s, size_t sw)
+{
+  size_t  i;
+  size_t  len;
+  char    *c;
+
+  i = 0;
+  len = ft_strlen(s);
+  c = (char *)malloc(sizeof(char) * (len + 1));
+  while (i < len && i < sw)
+  {
+    c[i] = s[i];
+    i++;
+  }
+  c[i] = '\0';
+  return c;
+}
