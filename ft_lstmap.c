524fa287213295fdbc8b9a237cdf90ed471032a1
hallfana <hallfana@proton.me>
2024-11-05 16:58:01 +0100

    auto update

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: samberna <samberna@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 16:32:17 by samberna          #+#    #+#             */
-/*   Updated: 2024/11/05 16:54:07 by samberna         ###   ########.fr       */
+/*   Updated: 2024/11/05 16:57:56 by samberna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,16 @@
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_list;
+	t_list	*head;
 
 	if (!lst || !f || !del)
 		return (NULL);
 	new_list = ft_lstnew(f(lst->content));
+	head = new_list;
 	while (lst && lst->next)
 	{
 		new_list->next = ft_lstnew(f(lst->next->content));
 		lst = lst->next;
 	}
-	return (new_list);
+	return (head);
 }
