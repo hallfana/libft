358b13e0308c98280317afb4c8b020585ceefd15
hallfana <hallfana@proton.me>
2024-11-05 15:33:31 +0100

    auto update

--- ft_lstsize.c
+++ ft_lstsize.c
@@ -1,21 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstadd_front.c                                  :+:      :+:    :+:   */
+/*   ft_lstsize.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: samberna <samberna@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/05 15:22:30 by samberna          #+#    #+#             */
-/*   Updated: 2024/11/05 15:29:26 by samberna         ###   ########.fr       */
+/*   Created: 2024/11/05 15:31:13 by samberna          #+#    #+#             */
+/*   Updated: 2024/11/05 15:33:27 by samberna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstadd_front(t_list **lst, t_list *new)
+int	ft_lstsize(t_list *lst)
 {
-	if (!new || !lst)
-		return ;
-	new->next = *lst;
-	*lst = new;
+	size_t	i;
+
+	i = 0;
+	while (lst->next != NULL)
+	{
+		lst = lst->next;
+		i++;
+	}
+	return (i);
 }
