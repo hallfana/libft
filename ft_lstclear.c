2d6124178812131a185b90eec86947b5ed180ff2
hallfana <hallfana@proton.me>
2024-11-05 16:16:08 +0100

    auto update

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: samberna <samberna@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 16:04:01 by samberna          #+#    #+#             */
-/*   Updated: 2024/11/05 16:14:24 by samberna         ###   ########.fr       */
+/*   Updated: 2024/11/05 16:16:05 by samberna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,6 @@ void	ft_lstclear(t_list **lst, void (*del)(void*))
 	if ((*lst)->next != NULL)
 		ft_lstclear(&(*lst)->next, del);
 	del((*lst)->content);
+	free(*lst);
 	*lst = NULL;
 }
