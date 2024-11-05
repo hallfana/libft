8f633c1cfd8b5cba91c80907809b701d4fff032d
hallfana <hallfana@proton.me>
2024-11-05 16:08:28 +0100

    auto update

---
+++ ft_lstclear.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: samberna <samberna@student.42perpignan.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/05 16:04:01 by samberna          #+#    #+#             */
+/*   Updated: 2024/11/05 16:08:23 by samberna         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+ft_lstclear(t_list **lst, void (*del)(void*))
+{
+	if ((*lst)->next != NULL)
+		ft_lstclear(&(*lst)->next, del);
+	ft_lstdelone(*lst, del);
+}
