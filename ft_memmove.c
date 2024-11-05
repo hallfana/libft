220aa8ad523aee3179ad2a33bfa342c945098137
hallfana <hallfana@proton.me>
2024-11-05 01:04:34 +0100

    auto update

--- ft_memmove.c
+++ ft_memmove.c
@@ -4,18 +4,20 @@ void  *ft_memmove(void *dest, const void *src, size_t n)
 {
   unsigned char *d;
   unsigned char *s;
-  size_t           i;
+  size_t        i;
 
+  if (dest == 0 && src == 0)
+    return (NULL);
   d = (unsigned char*)dest;
   s = (unsigned char*)src;
   i = 0;
   if (dest > src)
   {
-    while (n != 0)
-    {
-      n--;
+    while (--n != 0)
+    //{
+      //n--;
       d[n] = s[n];
-    }
+    //}
   }
   else
   {
