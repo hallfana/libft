fd9a6358ea8fc21c17e32a3b845ff00d5e7ce4d2
hallfana <hallfana@proton.me>
2024-11-05 01:17:35 +0100

    auto update

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -5,10 +5,10 @@ int ft_strncmp(const char *s1, const char *s2, size_t n)
   size_t  i;
 
   i = 0;
-  while (s1[i] && s2[i] && s1[i] == s2[i] && i < n)
+  while ((unsigned char)s1[i] && (unsigned char)s2[i] && (unsigned char)s1[i] == (unsigned char)s2[i] && i < n)
     i++;
   if (i < n)
-    return (unsigned int)(s1[i] - s2[i]);
+    return ((unsigned char)s1[i] - (unsigned char)s2[i]);
   else
     return (0);
 }
