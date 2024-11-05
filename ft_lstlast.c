5c94e8f87b03d0967b1e369e316a9f003396a24d
hallfana <hallfana@proton.me>
2024-11-05 15:38:37 +0100

    auto update

--- ft_lstlast.c
+++ ft_lstlast.c
@@ -6,7 +6,7 @@
 /*   By: samberna <samberna@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 15:36:15 by samberna          #+#    #+#             */
-/*   Updated: 2024/11/05 15:37:44 by samberna         ###   ########.fr       */
+/*   Updated: 2024/11/05 15:38:33 by samberna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 t_list	*ft_lstlast(t_list *lst)
 {
+	if (!lst)
+		return (lst);
 	while (lst->next)
 		lst = lst->next;
 	return (lst);
