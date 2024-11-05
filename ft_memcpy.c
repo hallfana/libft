8fe3798079b3a7193e3112a2ef584d616de31e9c
hallfana <hallfana@proton.me>
2024-11-05 01:03:14 +0100

    auto update

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,6 +6,8 @@ void  *ft_memcpy(void  *dest, const void *src, size_t n)
   unsigned char *d;
   unsigned char *s;
 
+  if (dest == 0 && src == 0)
+    return (NULL);
   i = 0;
   d = (unsigned char*)dest;
   s = (unsigned char*)src;
