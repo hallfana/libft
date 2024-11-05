62714c9f013c478ab9ed055cd4f2142be9fb3c21
hallfana <hallfana@proton.me>
2024-11-05 01:22:56 +0100

    auto update

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -7,8 +7,11 @@ size_t  ft_strlcpy(char *dst, const char *src, size_t dsize)
   i = 0;
   if (dsize > 0)
   {
-    while (++i < dsize - 1 && src[i])
+    while (i < dsize - 1 && src[i])
+    {
       dst[i] = src[i];
+      i++;
+    }
     dst[i] = '\0';
   }
   return (ft_strlen(src));
