928c71c2bddf7c2158a4ff791ee98546fb688307
hallfana <hallfana@proton.me>
2024-11-05 15:20:47 +0100

    auto update

--- libft.h
+++ libft.h
@@ -6,6 +6,16 @@
 
 # define SIZE_MAX 18446744073709551615
 
+typedef struct s_list
+{
+void *content;
+
+struct s_list *next;
+}
+
+
+t_list;
+
 void    ft_bzero(void *s, size_t n);
 void    *ft_memccpy(void  *dest, const void *src, int c, size_t n);
 void    *ft_memchr(const void *s, int c, size_t n);
@@ -40,4 +50,5 @@ void    ft_putchar_fd(char c, int fd);
 void    ft_putstr_fd(char *s, int fd);
 void    ft_putendl_fd(char *s, int fd);
 void    ft_putnbr_fd(int n, int fd);
+t_list *ft_lstnew(void *content);
 #endif
