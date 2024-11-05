19020ec2f5617bf47f5a7ac9a21f410a63e95fa2
hallfana <hallfana@proton.me>
2024-11-05 15:48:30 +0100

    auto update

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: samberna <samberna@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 15:42:07 by samberna          #+#    #+#             */
-/*   Updated: 2024/11/05 15:47:06 by samberna         ###   ########.fr       */
+/*   Updated: 2024/11/05 15:48:24 by samberna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	if (!lst)
-		return ;
+		return ft_lstnew(new->content);
 	while ((*lst)->next)
 		*lst = (*lst)->next;
 	(*lst)->next = new;
