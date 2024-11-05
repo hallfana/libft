6216fcd57c3dd0a1f2ac9d6b201a68f32000f8ce
hallfana <hallfana@proton.me>
2024-11-05 15:45:27 +0100

    auto update

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -1,22 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstlast.c                                       :+:      :+:    :+:   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: samberna <samberna@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/05 15:36:15 by samberna          #+#    #+#             */
-/*   Updated: 2024/11/05 15:41:00 by samberna         ###   ########.fr       */
+/*   Created: 2024/11/05 15:42:07 by samberna          #+#    #+#             */
+/*   Updated: 2024/11/05 15:45:17 by samberna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstlast(t_list *lst)
+void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	if (!lst)
 		return (lst);
-	while (lst->next)
-		lst = lst->next;
+	while ((*lst)->next)
+		lst = (*lst)->next;
+	(*lst)->next = new;
 	return (lst);
 }
